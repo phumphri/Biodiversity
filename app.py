1e1d3eb399e16880233466d0d94460f8a6be13e3
pvhumphr <pvhumphr@usc.edu>
2018-05-28 12:01:36 -0700

    App.py connection string.

--- app.py
+++ app.py
@@ -303,5 +303,6 @@ def samples(sample):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(environ.get("PORT", 5000)))
 
